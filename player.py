1bccc14d12f49467ec770019f0d644a19b5682d9
RayGerena <raygerena23@gmail.com>
2025-06-08 01:34:05 -0400

    added timer to prevent rapid fire

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.shot_timer = 0
   
   def draw(self, screen):
     pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -32,6 +33,8 @@ class Player(CircleShape):
     if keys[pygame.K_SPACE]:
       self.shoot()
 
+    self.shot_timer -= dt
+
   def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
     right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -41,5 +44,8 @@ class Player(CircleShape):
     return [a, b, c]
 
   def shoot(self):
+    if self.shot_timer > 0:
+      return
+    self.shot_timer = PLAYER_SHOOT_COOLDOWN
     bullet = Shot(self.position.x, self.position.y)
     bullet.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
